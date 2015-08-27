71b82637bd60afa255c9a0c5b6bc44b5251f00a8
Carryl Sorene <carrylsorene@gmail.com>
2015-08-27 20:21:04 +0100

    If statement to calculate stops on one line

--- mta.rb
+++ mta.rb
@@ -1,37 +1,69 @@
 train_lines = {
-  line_n: ['TS', '34', '28N', '23N', 'USN', '8N'],
-  line_l: ['8L', '6', 'USL', '3', '1'],
-  line_6: ['GC', '33', '286', '236', 'US6', 'AP']
+  :line_n => ['TS', '34', '28', '23', 'US', '8'],
+  :line_l => ['8', '6', 'US', '3', '1'],
+  :line_6 => ['GC', '33', '28', '23', 'US', 'AP']
 }
 
-# changes = 0
-# stop_number = 0
+# changes = 'no' #not required since there's only 1 on these 3 lines, but in theory would define
+stop_number = 0
 
-puts 'Which line do you want to use?'
-gets.downcase
-line =
+puts 'Which line do you want to start on?'
+line_start_input = gets.chomp.downcase
 puts 'Which station are you starting from?'
-gets.downcase
-station_start = 
+station_start = gets.chomp.downcase
 puts 'Which station are you going to?'
-gets.downcase
-station_end =
+station_end = gets.chomp.downcase
+puts 'Which line do you want to finish on?'
+line_end_input = gets.chomp.downcase
 
-# if 
-#   line = n
-#   stop_number = station_end - station_start
-# elsif
-#   line = l
-#   stop_number = station_end - station_start
-# elsif
-#   line = 6 
-#   stop_number = station_end - station_start
-# end
+#determine correct line in hash depending on user input 
 
-# stop_number = station_end - station_start
-puts 'Your journey will take' stop_number 'stops'
+if line_start_input == 'n'
+   line_start = :line_n
+elsif line_start_input == 'l'
+   line_start = :line_l
+elsif line_start_input == '6'
+   line_start = :line_6
+end
 
-changes = 'one'
+# calculate stops - if they're on same line
 
-# while route(var) includes 'Union Square' ?
-puts ' and will include one change.'
+if line_start == :line_n && train_lines[:line_n].index.(station_start)
+  stops = train_lines[:line_n].index.(station_end) - train_lines[:line_n].index.(station_start)
+  # stops = stops.abs
+  puts 'Your journey will include ' + stops.to_s + ' stops'
+elsif
+  line_start == :line_l && train_lines[:line_l].index.(station_start)
+  stops = train_lines[:line_l].index.(station_end) - train_lines[:line_l].index.(station_start)
+  # stops = stops.abs
+  puts 'Your journey will include ' + stops.to_s + ' stops'
+elsif 
+  line_start == :line_6 && train_lines[:line_6].index.(station_start)
+  stops = train_lines[:line_6].index.(station_end) - train_lines[:line_6].index.(station_start)
+  # stops = stops.abs
+  puts 'Your journey will include ' + stops.to_s + ' stops'
+end
+
+#Find index of intersection on each array
+
+# start_us_index = train_lines(line).index('US') # use this var to calc stops from intersection - add to no of stops either side
+# end_us_index = train_lines(line).index('US')
+
+# if line_start == line_n && train_lines[:line_n].index.(station_start)
+
+
+# calculate stops- if they change lines
+
+
+
+
+
+# start_index = train_lines(line).index(station_start)
+# end_index = train_lines(line).index(station_end)
+
+
+
+
+# puts 'Your journey will include ' + stop_number.to_s + ' stops'
+# changes = 'one'
+# puts ' and will include one change.'
